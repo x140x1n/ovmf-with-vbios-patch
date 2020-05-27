c5c708503cecdb25d757d78ec6072ec493b95607
Edmund Wu <fangkazuto@gmail.com>
2020-05-27 12:30:34 -0400

    v2

--- bin/build.sh
+++ bin/build.sh
@@ -2,5 +2,5 @@
 
 mkdir -p build
 docker build -t ovmf-vbios-patch docker-build
-docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh gtx-1050ti-maxq.rom
+docker run --rm -v "$PWD/build:/build" -v "$PWD/roms:/roms" ovmf-vbios-patch /ovmf/compile-ovmf.sh 1050_ti_maxq.rom
 tar -czf build/ovmf-vbios-patched.tgz build/OVMF*
